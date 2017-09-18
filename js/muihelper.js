f1a770c4aef2fcd1011f08c165ad1ab6499c1967
M.js <1121302139@qq.com>
2017-09-18 18:10:19 +0800

    新增

--- js/muihelper.js
+++ js/muihelper.js
@@ -381,3 +381,11 @@ window.addEventListener("tap", function() {
 		plus.navigator.setStatusBarStyle("light")
 	}
 })
+
+window.addEventListener('resize', function() {
+	if(document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
+		window.setTimeout(function() {
+			document.activeElement.scrollIntoViewIfNeeded();
+		}, 0);
+	}
+})
